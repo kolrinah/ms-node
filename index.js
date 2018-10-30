75d69e4ac46419240e5b7da0678e8723dc9074ce
Hector <kolrinah@gmail.com>
2018-10-29 20:11:23 -0400

    service-account for read spreadsheets

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ mongoose.connect('mongodb://localhost:27017/cf_test',
         });
     }
 });
+
+
+const GoogleSpreadsheet = require('google-spreadsheet')
+const { promisify } = require('util')
+
+const credentials = require('./service-account.json')
+
+const SPREADSHEET_ID = `1NPQlqMeuzD3lg5SL_PmlFtEQPKye-u7V7gdIjf6iso0`
+async function accessSpreadsheet() {
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
+  await promisify(doc.useServiceAccountAuth)(credentials)
+  const info = await promisify(doc.getInfo)()
+  console.log(`Loaded doc: ` + info.title + ` by ` + info.author.email)
+  const sheet = info.worksheets[0]
+  console.log(
+    `sheet 1: ` + sheet.title + ` ` + sheet.rowCount + `x` + sheet.colCount
+  )
+}
+
+accessSpreadsheet()
